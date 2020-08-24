f7a8a663a9f4766c5209070105a17205728ef29f
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-08-24 16:15:36 -0300

    first commit

---
+++ hello-world-python-sh/hello.py
@@ -0,0 +1,5 @@
+from colorama import Fore
+from colorama import init
+
+init()
+print(Fore.GREEN+"Hello"+Fore.MAGENTA+" World"+Fore.YELLOW+" in"+Fore.CYAN+" Python"+Fore.RESET+"!")
---
+++ hello-world-python-sh/hello.sh
@@ -0,0 +1,2 @@
+echo "Hello World in sh!"
+python3 hello.py
