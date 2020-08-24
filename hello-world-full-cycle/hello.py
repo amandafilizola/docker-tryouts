bef6f5d8d5ab6559404052e40db7f405fa0b43c9
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-08-24 16:19:50 -0300

    new folder with string changes

---
+++ hello-world-full-cycle/hello.py
@@ -0,0 +1,5 @@
+from colorama import Fore
+from colorama import init
+
+init()
+print(Fore.GREEN+"Eu"+Fore.MAGENTA+" sou"+Fore.YELLOW+" Full"+Fore.CYAN+" Cycle"+Fore.RESET+"!")
---
+++ hello-world-full-cycle/hello.sh
@@ -0,0 +1,2 @@
+echo "Eu sou Full Cycle!"
+python3 hello.py
